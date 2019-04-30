9c11545d2f531172a5e615fb7c89d412f2410fb2
EasyMetaData <ddym@redrocktx.com>
2019-04-30 10:42:08 -0500

    Add files via upload

---
+++ SourceCodeStats/GetStats.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SourceCodeStats
+{
+    public class GetStats
+    {
+        public static Dictionary<string, int> dictUsing = new Dictionary<string, int>();
+        public static Dictionary<string, int> dictNamespaces = new Dictionary<string, int>();
+        public static Dictionary<string, int> dictClasses = new Dictionary<string, int>();
+        public static Dictionary<string, int> dictMethods = new Dictionary<string, int>();
+        public static Dictionary<string, int> dictWords = new Dictionary<string, int>();
+        public static Dictionary<string, int> dictComments = new Dictionary<string, int>();
+
+        public static void CountUsing(string filename)
+        {
+            var lines = File.ReadAllLines(@filename);
+
+            try
+            {
+                foreach (var line in lines)
+                {
+                    if (line.Contains("using"))
+                    {
+                        var cleanLine = line.Replace(";", "");
+
+                        var items = cleanLine.Split(new char[] { ' ' }).ToList();
+
+                        var nKey = 1;
+
+                        if (items.Count > 1 )
+                        {
+                            var bResult = dictUsing.TryGetValue(items[1], out nKey);
+
+                            if (bResult)
+                            {
+                                dictUsing[items[1]] = nKey + 1;
+                            }
+                            else
+                            {
+                                dictUsing.Add(items[1], 1);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ex: {0}", ex.InnerException);
+                throw ex;
+            }
+
+        }
+
+        public static void CountNamespaces(string filename)
+        {
+            var lines = File.ReadAllLines(@filename);
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("namespace"))
+                {
+                    var cleanLine = line.Replace(";", "");
+
+                    string hit = cleanLine.Split(new string[] { "namespace" }, StringSplitOptions.None).Last();
+
+                    var nKey = 1;
+
+                    var bResult = dictNamespaces.TryGetValue(hit, out nKey);
+
+                    if (bResult)
+                    {
+                        dictNamespaces[hit] = nKey + 1;
+                    }
+                    else
+                    {
+                        dictNamespaces.Add(hit, 1);
+                    }
+                }
+
+            }
+            
+        }
+
+        public static void CountClasses(string filename)
+        {
+            var lines = File.ReadAllLines(@filename);
+
+            //var dictUsing = new Dictionary<string, int>();
+
+            //Console.WriteLine(filename + ": Classes");
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("class "))
+                {
+                    var cleanLine = line.Replace(";", "");
+
+                    string hit = cleanLine.Split(new string[] { "class" }, StringSplitOptions.None).Last();
+
+                    var nKey = 1;
+
+                    var bResult = dictClasses.TryGetValue(hit, out nKey);
+
+                    if (bResult)
+                    {
+                            dictClasses[hit] = nKey + 1;
+                    }
+                    else
+                    {
+                        dictClasses.Add(hit, 1);
+                    }    
+
+                }
+
+            }
+
+        }
+
+        public static void CountComments(string filename)
+        {
+            var lines = File.ReadAllLines(@filename);
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("// ") || line.Contains("///"))
+                {
+                    var cleanLine = line.Replace(";", "");
+
+                    string hit = (cleanLine.Split(new string[] { "//", "///" }, StringSplitOptions.None).Last()).Replace("/","");
+
+                    var nKey = 1;
+
+                    var bResult = dictComments.TryGetValue(hit, out nKey);
+
+                    if (bResult)
+                    {
+                        dictComments[hit] = nKey + 1;
+                    }
+                    else
+                    {
+                        dictComments.Add(hit, 1);
+                    }
+
+                }
+
+            }
+
+            // Console.WriteLine("Count {0}", dictUsing.Count);
+        }
+
+        public static void CountWords(string filename)
+        {
+            var lines = File.ReadAllLines(@filename);
+
+            foreach (var line in lines)
+            {
+                var cleanLine = line.Replace(";", "");
+
+                var punctuation = cleanLine.Where(Char.IsPunctuation).Distinct().ToArray();
+                var words = cleanLine.Split().Select(x => x.Trim(punctuation));
+
+                foreach (var hit in words)
+                {
+                    if (!string.IsNullOrEmpty(hit))
+                    {
+                        var nKey = 1;
+
+                        var bResult = dictWords.TryGetValue(hit.ToString(), out nKey);
+
+                        if (bResult)
+                        {
+                            //Console.WriteLine("Found:   {0}; {1}", hit, nKey);
+                            dictWords[hit.ToString()] = nKey + 1;
+                        }
+                        else
+                        {
+                            dictWords.Add(hit.ToString(), 1);
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+
+        public static void CountMethods(string filename)
+        {
+            var lines = File.ReadAllLines(@filename);
+
+            string[] str = { "void","public", "private", "protected", "static", "final", "native", "synchronized", "abstract", "transient" };
+
+            foreach (var line in lines)
+            {
+                var cleanLine = line.Replace(";", "");
+
+                var check = (from strF in str
+                            where cleanLine.Contains(strF)
+                            select strF).ToList();
+
+                if (check.Count > 0)
+                {
+                    string hitKey = cleanLine.Split(str, StringSplitOptions.None).First();
+                    string hit = cleanLine.Split(str , StringSplitOptions.None).Last();
+
+                    var nKey = 1;
+
+                    var bResult = dictMethods.TryGetValue(hit, out nKey);
+
+                    if (bResult)
+                    {
+                        //Console.WriteLine("Found:   {0}; {1}", hit, nKey);
+                        dictMethods[hit] = nKey + 1;
+                    }
+                    else
+                    {
+                        dictMethods.Add(hit, 1);
+                    }
+
+                }
+
+            }
+
+            // Console.WriteLine("Count {0}", dictUsing.Count);
+        }
+
+        public static void ToCSV(Dictionary<string, int> dict, string outPath, string outFile)
+        {
+            var outFullPath = @Path.Combine(outPath, outFile);
+
+            string csv = "Item; Count\r\n";
+            csv += String.Join(
+                Environment.NewLine,
+                dict.OrderByDescending(x => x.Value).Distinct().Select(d => d.Key + ";" + d.Value + ";"));
+
+            System.IO.File.WriteAllText(outFullPath, csv);
+        }
+
+    }
+}
---
+++ SourceCodeStats/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SourceCodeStats
+{
+    class Program
+    {
+        public static List<string> FileList = new List<string>();
+        public static List<string> DirectoryList = new List<string>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In Path: " + @args[0]);
+
+            //var files = Directory.GetFiles(@args[0], "*.cs");
+
+            BuildDirectoryList(@args[0], "*.*");
+
+            foreach (var file in FileList)
+            {
+                var fo = new FileInfo(file);
+
+                if (fo.Extension == ".cs")
+                {
+                    Console.WriteLine("File: {0}", file);
+
+                    GetStats.CountUsing(file);
+                    GetStats.CountNamespaces(file);
+                    GetStats.CountClasses(file);
+                    GetStats.CountMethods(file);
+                    GetStats.CountWords(file);
+                    GetStats.CountComments(file);
+                }
+            }
+
+
+            GetStats.ToCSV(GetStats.dictNamespaces, @args[1], "CountNamespaces.csv");
+            GetStats.ToCSV(GetStats.dictClasses, @args[1], "CountClasses.csv");
+            GetStats.ToCSV(GetStats.dictUsing, @args[1], "CountUsing.csv");
+            GetStats.ToCSV(GetStats.dictMethods, @args[1], "CountMethods.csv");
+            GetStats.ToCSV(GetStats.dictWords, @args[1], "CountWords.csv");
+            GetStats.ToCSV(GetStats.dictComments, @args[1], "CountComments.csv");
+
+        }
+
+        public static void BuildDirectoryList(string dir, string _pattern)
+        {
+            try
+            {
+
+                var files = Directory.GetFiles(dir, _pattern);
+
+                lock (FileList)
+                {
+                    FileList.AddRange(files);
+                }
+
+                // get the file attributes for file or directory
+                var directories = Directory.GetDirectories(dir, _pattern);
+
+                DirectoryList.AddRange(directories);
+
+                    foreach (string directory in directories)
+                    {
+                        BuildDirectoryList(directory, _pattern);
+                    }
+
+
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                throw UAEx;
+                //Console.WriteLine(UAEx.Message);
+
+                // Console.WriteLine(MetaDiver_Core.logging.HandleError(UAEx, "directorylist UnauthorizedAccessException", "BuildDirectoryList: " + dir));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+
+
+
+    }
+}
